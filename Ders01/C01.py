e4af504a8ede85c9e5e27533fb6cd25cfd72bd10
Fatih Turker <fthturker80@gmail.com>
2023-03-01 22:40:56 +0300

    18.02.23

---
+++ Ders01/C01.py
@@ -0,0 +1,11 @@
+print('Mustafa Murat Coşkun')
+print("Mustafa Murat Coşkun")
+print("""Mustafa Murat Coşkun""")
+a='ali'
+print(a[0] ,a[1],a[2])
+
+print(a[-2])
+
+a = "Python Programlama Dili"
+print(a[4:10])
+print('Fatih\nTürker')
